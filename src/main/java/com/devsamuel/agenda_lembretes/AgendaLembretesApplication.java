3f2cd0089215ee7e044027954f9a4fcae3cd7844
Samuel Gustavo <dev.samuelgustavo@gmail.com>
2026-04-09 09:09:18 -0300

    Organização Projeto

---
+++ src/main/java/com/devsamuel/agenda_lembretes/AgendaLembretesApplication.java
@@ -0,0 +1,13 @@
+package com.devsamuel.agenda_lembretes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaLembretesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaLembretesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsamuel/agenda_lembretes/AgendaLembretesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsamuel.agenda_lembretes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaLembretesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
